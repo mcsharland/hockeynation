a06fe2621b9b59b813e7c73b83495ebe854a4099
mcsharland <mcsharland@mail.csuchico.edu>
2026-05-23 22:24:25 -0700

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,30 @@
 const path = require("path");
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === "production";
-  return {
-      mode: argv.mode || "development",
-      devtool: isProduction ? false : "source-map",
-      entry: {
-        contentScript: "./src/contentScript.ts",
-        interceptor: "./src/interceptor.ts",
-      },
-      module: {
-        rules: [
-          {
-            test: /\.tsx?$/,
-            use: "ts-loader",
-            exclude: /node_modules/,
-          },
-        ],
-      },
-      resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-      },
-      output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-      },
-    };
-
+	const isProduction = argv.mode === "production";
+	return {
+		mode: argv.mode || "development",
+		devtool: isProduction ? false : "source-map",
+		entry: {
+			contentScript: "./src/contentScript.ts",
+			interceptor: "./src/interceptor.ts",
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: "ts-loader",
+					exclude: /node_modules/,
+				},
+			],
+		},
+		resolve: {
+			extensions: [".tsx", ".ts", ".js"],
+		},
+		output: {
+			filename: "[name].js",
+			path: path.resolve(__dirname, "dist"),
+			clean: true,
+		},
+	};
 };
